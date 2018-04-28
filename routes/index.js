599c690e16edd67d4356088ce977f63f4179d739
Ankita Khurana <ankita0096@gmail.com>
2018-04-28 18:20:05 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,22 @@
 const route = require('express').Router();
 
 route.get('/',function (req,res) {
-    res.send('Hi There');
+    res.send('Hi there, use /api/company or /api/student/ to use the APIs :) ');
 });
 
+
+route.get('/api/',function (req,res) {
+    res.send('Hi there, use /company/ or /student/ to use the APIs:) ');
+});
+
+
+route.get('/api/student/',function (req,res) {
+    res.send('Try /all to get all students :)');
+});
+
+
+route.get('/api/company/',function (req,res) {
+    res.send('Try /all to get all companies :)');
+});
 
 module.exports = route;
--- server.js
+++ server.js
@@ -2,14 +2,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-// const cookieParser = require('cookie-parser');
-// const expressSession = require('express-session');
-
-//const passport = require('./auth/passport');
-
 const app = express();
 
+// Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -17,31 +12,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 mongoose.connect("mongodb://localhost/placements");
 const db = mongoose.connection;
 
-
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-//
-
-
-
-// app.use(cookieParser('my super secret'));
-// app.use(expressSession({
-//     secret: 'my super secret',
-//     resave: false,
-//     saveUninitialized: false
-// }));
-//
-// app.use(passport.initialize());
-// app.use(passport.session());
-
+// Routes
 app.use('/api', require('./routes/api'));
 app.use('/', require('./routes/index'));
 app.use('/', express.static(__dirname + "/static"));
 
-
-
+// App Listening to port 3456
 app.listen(3456, function () {
     console.log("Server started on http://localhost:3456");
 });
